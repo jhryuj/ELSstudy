80ef7d1b626e995835e6b393491a437d4128d80e
joshryu <joshryu0425@gmail.com>
2019-08-17 15:09:01 -0700

    bf

--- Preprocessing/movedata.m
+++ Preprocessing/movedata.m
@@ -32,6 +32,7 @@ for subjN = 1:size(subjlist_data,1)
     %% find kidmid file. unzip in the folder
     kidmid_niigz = dir(fullfile(subjrawdata_dir,'kidmid*.nii.gz'));
     if isempty(kidmid_niigz)
+        disp([subjID 'No kidmid nifti']);
         subjlist_data.DirCheck{subjN} = 'No kidmid nifti';
         continue
     end
@@ -43,6 +44,7 @@ for subjN = 1:size(subjlist_data,1)
             gunzip(fullfile(kidmid_niigz.folder,kidmid_niigz.name),...
                 subjdata_dir)
         catch error
+            disp([subjID 'Cannot unzip kidmid nifti']);
             subjlist_data.DirCheck{subjN} = 'Cannot unzip kidmid nifti';
             continue
         end
@@ -51,6 +53,7 @@ for subjN = 1:size(subjlist_data,1)
     %% copy T1 acpc
     T1_niigz = dir(fullfile(subjrawdata_dir,'T1*raw_acpc.nii.gz'));
     if isempty(T1_niigz)
+        disp([subjID 'No T1 acpc nifti']);
         subjlist_data.DirCheck{subjN} = 'No T1 acpc nifti';
         continue
     end
@@ -60,6 +63,7 @@ for subjN = 1:size(subjlist_data,1)
             gunzip(fullfile(T1_niigz.folder,T1_niigz.name),...
                 subjdata_dir)
         catch error
+            disp([subjID 'Cannot unzip T1 nifti']);
             subjlist_data.DirCheck{subjN} = 'Cannot unzip T1 nifti';
             continue
         end
@@ -69,7 +73,8 @@ for subjN = 1:size(subjlist_data,1)
     subj_behv_dir   = fullfile(subjdata_dir,'Behavioral');
     subj_rawbeh     = fullfile(subjrawevfile_dir,[subjID '*']); 
     
-    if ~isempty(dir(subj_rawbeh))
+    if isempty(dir(subj_rawbeh))
+        disp([subjID 'No behavior data']);
         subjlist_data.DirCheck{subjN} = 'No behavior data';
     else
         copyfile subj_rawbeh subj_behv_dir
